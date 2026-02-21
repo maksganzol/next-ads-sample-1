e7a879fb95f644098d536e39e9b9aa1ca476d218
Kseniia <xeniyyaa@MacBook-Air-Kseniia.local>
2026-02-21 16:31:50 +0200

    Update Website ID

--- components/NextAdsScript.tsx
+++ components/NextAdsScript.tsx
@@ -3,5 +3,6 @@
 import Script from "next/script";
 
 export default function NextAdsScript() {
-  return <Script src="https://api-dev.next-ads-server-dev.com/script/tag.js?websiteId=63" strategy="afterInteractive" />;
+  return <Script src="https://api-dev.next-ads-server-dev.com/script/tag.js?websiteId=6" 
+  strategy="afterInteractive" />;
 }
